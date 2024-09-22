732350393d7cebc71465221c6474bdd6447b4267
Sourav kumar jha <souravkumarjha301@gmail.com>
2024-09-22 23:01:21 +0530

    Hero section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <main className="overflow-x-hidden antialiased text-neutral-800">
         <Navbar />
+        <Hero/>
       </main>
     </>
   )
